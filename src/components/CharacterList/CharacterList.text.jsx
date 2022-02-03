dd8ab35990d517c03e0762b9e0e0ade291597bc8
zcaldwell <mintyst.john@gmail.com>
2022-02-03 15:03:36 -0800

    working on testing

--- src/components/CharacterList/CharacterList.text.jsx
+++ src/components/CharacterList/CharacterList.text.jsx
@@ -1,5 +1,9 @@
-import { render } from 'react-dom';
+import { render } from '@testing-library/react';
+import CharacterList from './CharacterList';
 
-test('testing how many list items render', () => {
-  render();
+test('testing how many list items render', async () => {
+  render(<CharacterList />);
+
+  const characters = await screen.findAllByRole('listitem');
+  expect(characters).toHaveLength(100);
 });
